7a80674d5353cb174455de012db9c700d68e4426
Stefano <stefano.turcarelli@icloud.com>
2023-12-07 08:40:37 -0600

    Organize functions

--- assets/script/app.js
+++ assets/script/app.js
@@ -142,7 +142,7 @@ const usedWordsArray = [];
 const gameForm = select("#game-form");
 const buttonAbout = select(".header-menu-btn-1");
 const buttonCoins = select(".header-menu-btn-2");
-const buttonGems = select(".header-menu-btn-3");
+const buttonScores = select(".header-menu-btn-3");
 const buttonMenu = select(".header-menu-btn-4");
 const buttonStart = select(".game-start-btn");
 const buttonReset = select(".game-reset-btn");
@@ -153,26 +153,15 @@ const scoresArrayDisplay = select(".scores-array-container");
 const statsDisplay = select(".stats");
 const selectTimeBtns = selectAll(".select-time-btn");
 
-// Overlay
 const overlayAbout = select(".overlay-about");
 const overlayCoins = select(".overlay-coins");
-const overlayGems = select(".overlay-gems");
+const overlayScores = select(".overlay-scores");
 const overlayMenu = select(".overlay-menu");
 const overlayCloseBtns = selectAll(".overlay-content-close-btn");
 
 const gameWords = select(".game-words");
 const gamePercentage = select(".game-percentage");
 const gameDateScore = select(".game-date-score");
-
-let seconds = 99;
-// let selectedTime = 99;
-
-let points = 0;
-let percentage = 0;
-let currentWord = "";
-let currentWordIndex = 0;
-
-const scoresArray = [];
 
 const backgroundMusic = new Audio("./assets/media/sound/music.mp3");
 const endingSound = new Audio("./assets/media/sound/ending.mp3");
@@ -182,15 +171,150 @@ backgroundMusic.volume = 0.1;
 endingSound.volume = 0.4;
 buttonSound.volume = 0.3;
 
-function playKeySound() {
-  const audio = new Audio("./assets/media/sound/key.mp3");
-  audio.volume = 0.2;
-  audio.play();
+let timer;
+let seconds = 99;
+let selectedSeconds = 99;
+
+let points = 0;
+let percentage = 0;
+let currentWord = "";
+let currentWordIndex = 0;
+
+const scoresArray = [];
+
+const allButtons = selectAll(".button");
+
+// ! ---------------------------------------------------------------------------
+// ! Buttons
+// ! ---------------------------------------------------------------------------
+
+allButtons.forEach((btn) => {
+  onEvent("click", btn, () => {
+    buttonSound.play();
+  });
+});
+
+overlayCloseBtns.forEach((btn) => {
+  onEvent("click", btn, () => {
+    hideOverlayAbout();
+    hideOverlayCoins();
+    hideOverlayScores();
+    hideOverlayMenu();
+  });
+});
+
+onEvent("click", buttonStart, () => {
+  seconds = selectedSeconds;
+  inputWord.focus();
+  startGame();
+  statsDisplay.style.opacity = "1";
+});
+
+onEvent("click", buttonReset, () => {
+  inputWord.value = "";
+  resetGame();
+  gameTimer.textContent = seconds;
+});
+
+onEvent("click", buttonAbout, () => {
+  showOverlayAbout();
+});
+
+onEvent("click", buttonCoins, () => {
+  showOverlayCoins();
+});
+
+onEvent("click", buttonScores, () => {
+  showOverlayScores();
+});
+
+onEvent("click", buttonMenu, () => {
+  showOverlayMenu();
+});
+
+selectTimeBtns.forEach((btn) => {
+  // Set the time the user selected
+  onEvent("click", btn, () => {
+    selectedSeconds = btn.dataset.time;
+    gameTimer.textContent = selectedSeconds;
+  });
+  // Paint the time button the user selected
+  onEvent("click", btn, () => {
+    selectTimeBtns.forEach((btn) => {
+      btn.classList.remove("selected");
+    });
+    btn.classList.add("selected");
+  });
+});
+
+// ! ---------------------------------------------------------------------------
+// ! Words
+// ! ---------------------------------------------------------------------------
+
+onEvent("input", inputWord, () => {
+  compareWords();
+  gameWords.textContent = points;
+  gamePercentage.textContent = Math.round((points / wordsArray.length) * 100);
+});
+
+// ! ---------------------------------------------------------------------------
+// ! Functions
+// ! ---------------------------------------------------------------------------
+
+function resetGame() {
+  stopBackgroundMusic();
+  endingSound.play();
+  clearInterval(timer);
+  finalizeGame();
+  resetGameValues();
 }
 
-function stopBackgroundMusic() {
-  backgroundMusic.pause();
-  backgroundMusic.currentTime = 0;
+function startGame() {
+  inputWord.value = "";
+  backgroundMusic.play();
+  initializeGameDisplay();
+  onEvent("keydown", document, playKeySound);
+  startTimer();
+}
+
+function initializeGameDisplay() {
+  currentWord = printRandomWordFromArray();
+  currentWordIndex = wordsArray.indexOf(currentWord);
+  buttonStart.classList.add("hidden");
+  buttonReset.classList.remove("hidden");
+  gameTimer.style.visibility = "visible";
+  gameDateScore.textContent = "";
+  gameWords.textContent = "0";
+  gamePercentage.textContent = "0";
+  inputWord.style.opacity = "1";
+}
+
+function finalizeGame() {
+  inputWord.style.opacity = "0";
+  randomWordDisplay.textContent = "Game Over";
+  buttonStart.classList.remove("hidden");
+  buttonReset.classList.add("hidden");
+  gameTimer.style.visibility = "hidden";
+  document.removeEventListener("keydown", playKeySound);
+  resetGameValues();
+}
+
+function resetGameValues() {
+  seconds = selectedSeconds;
+  points = 0;
+  percentage = 0;
+  inputWord.value = "";
+}
+
+function endGame(timer) {
+  const timeString = getCurrentTimeString();
+  updateGameScore(timeString);
+
+  stopBackgroundMusic();
+  endingSound.play();
+
+  clearInterval(timer);
+  finalizeGame();
 }
 
 function printRandomWordFromArray() {
@@ -233,13 +357,18 @@ function hideOverlayAbout() {
   overlayAbout.classList.remove("visible");
 }
 
-onEvent("click", buttonAbout, () => {
-  buttonSound.play();
-  showOverlayAbout();
-});
+function playKeySound() {
+  const audio = new Audio("./assets/media/sound/key.mp3");
+  audio.volume = 0.2;
+  audio.play();
+}
+
+function stopBackgroundMusic() {
+  backgroundMusic.pause();
+  backgroundMusic.currentTime = 0;
+}
 
 // * Coins button and overlay
-
 function showOverlayCoins() {
   overlayCoins.classList.add("visible");
 }
@@ -248,28 +377,15 @@ function hideOverlayCoins() {
   overlayCoins.classList.remove("visible");
 }
 
-onEvent("click", buttonCoins, () => {
-  buttonSound.play();
-  showOverlayCoins();
-});
-
-// * Gems button and overlay
-
-function showOverlayGems() {
-  overlayGems.classList.add("visible");
+function showOverlayScores() {
+  overlayScores.classList.add("visible");
 }
 
-function hideOverlayGems() {
-  overlayGems.classList.remove("visible");
+function hideOverlayScores() {
+  overlayScores.classList.remove("visible");
 }
-
-onEvent("click", buttonGems, () => {
-  buttonSound.play();
-  showOverlayGems();
-});
 
 // * Menu button and overlay
-
 function showOverlayMenu() {
   overlayMenu.classList.add("visible");
 }
@@ -277,80 +393,6 @@ function showOverlayMenu() {
 function hideOverlayMenu() {
   overlayMenu.classList.remove("visible");
 }
-
-onEvent("click", buttonMenu, () => {
-  buttonSound.play();
-  showOverlayMenu();
-});
-
-overlayCloseBtns.forEach((btn) => {
-  onEvent("click", btn, () => {
-    buttonSound.play();
-    hideOverlayAbout();
-    hideOverlayCoins();
-    hideOverlayGems();
-    hideOverlayMenu();
-  });
-});
-
-// * Select time buttons
-
-let selectedSeconds = 99;
-
-selectTimeBtns.forEach((btn) => {
-  onEvent("click", btn, () => {
-    buttonSound.play();
-    selectedSeconds = btn.dataset.time;
-    gameTimer.textContent = selectedSeconds;
-  });
-});
-
-// Paint the time button the user selected
-selectTimeBtns.forEach((btn) => {
-  onEvent("click", btn, () => {
-    selectTimeBtns.forEach((btn) => {
-      btn.classList.remove("selected");
-    });
-    btn.classList.add("selected");
-  });
-});
-
-onEvent("click", buttonReset, () => {
-  inputWord.value = "";
-  buttonSound.play();
-  resetGame();
-  gameTimer.textContent = seconds;
-});
-
-function resetGame() {
-  stopBackgroundMusic();
-  endingSound.play();
-  clearInterval(timer);
-  finalizeGame();
-  resetGameValues();
-}
-
-function startGame() {
-  inputWord.value = "";
-  backgroundMusic.play();
-  initializeGameDisplay();
-  onEvent("keydown", document, playKeySound);
-  startTimer();
-}
-
-function initializeGameDisplay() {
-  currentWord = printRandomWordFromArray();
-  currentWordIndex = wordsArray.indexOf(currentWord);
-  buttonStart.classList.add("hidden");
-  buttonReset.classList.remove("hidden");
-  gameTimer.style.visibility = "visible";
-  gameDateScore.textContent = "";
-  gameWords.textContent = "0";
-  gamePercentage.textContent = "0";
-  inputWord.style.opacity = "1";
-}
-
-let timer;
 
 function startTimer() {
   timer = setInterval(() => {
@@ -365,25 +407,6 @@ function startTimer() {
 function updateGameTimer() {
   seconds--;
   gameTimer.textContent = seconds;
-}
-
-onEvent("click", buttonStart, () => {
-  seconds = selectedSeconds;
-  inputWord.focus();
-  buttonSound.play();
-  startGame();
-  statsDisplay.style.opacity = "1";
-});
-
-function endGame(timer) {
-  const timeString = getCurrentTimeString();
-  updateGameScore(timeString);
-
-  stopBackgroundMusic();
-  endingSound.play();
-
-  clearInterval(timer);
-  finalizeGame();
 }
 
 function getCurrentTimeString() {
@@ -420,28 +443,9 @@ function printScoresArray() {
   });
 }
 
-function finalizeGame() {
-  inputWord.style.opacity = "0";
-  randomWordDisplay.textContent = "Game Over";
-  buttonStart.classList.remove("hidden");
-  buttonReset.classList.add("hidden");
-  gameTimer.style.visibility = "hidden";
-  document.removeEventListener("keydown", playKeySound);
-  resetGameValues();
-}
-
-function resetGameValues() {
-  seconds = selectedSeconds;
-  points = 0;
-  percentage = 0;
-  inputWord.value = "";
-}
-
-onEvent("input", inputWord, () => {
-  compareWords();
-  gameWords.textContent = points;
-  gamePercentage.textContent = Math.round((points / wordsArray.length) * 100);
-});
+// ! ---------------------------------------------------------------------------
+// ! Functionality fixes
+// ! ---------------------------------------------------------------------------
 
 // Prevent the default form submission behavior
 onEvent("submit", gameForm, (event) => {
